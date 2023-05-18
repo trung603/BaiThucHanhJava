8a3f3fb9ca25accad7cf8f8d859d29902a53b9db
trung603 <lethetrung2@gmail.com>
2023-05-18 14:35:25 +0700

    BTH 18/05

---
+++ BaiThucHanhJava/BaiThucHanhLab4/src/main/java/BaiThucHanh/Bai2.java
@@ -0,0 +1,12 @@
+package BaiThucHanh;
+import java.util.LinkedList;
+import java.util.Scanner;
+public class Bai2 {
+    public static void main(String[] args) {
+        System.out.println("Nhap vao so phan tu cua LinkedList:");
+        Scanner sc = new Scanner(System.in);
+        int temp = sc.nextInt();
+       
+
+    }
+} 
